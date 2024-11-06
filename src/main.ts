58179cbc910cfa3ca6c276f8cca7a2fb3fe06a8f
拉布拉马 <2039203690@qq.com>
2024-11-06 22:58:49 +0800

    feat: 跨域配置

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // // 跨域
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ['http://localhost', 'http://localhost:8848'];
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error('跨域发生错误'), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
